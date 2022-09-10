02ba5d93a3f62a209eafd39ed8e7f13c28ce7438
shamil16 <chernovshamil6@gmail.com>
2022-09-10 09:53:11 +0600

    a

--- app/src/main/java/com/company/MainActivity.java
+++ app/src/main/java/com/company/MainActivity.java
@@ -2,13 +2,34 @@ package com.company;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameEntry;
+    private Button changeActivityButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        changeActivityButton=findViewById(R.id.btn_click);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                mNameEntry = findViewById (R.id.et_one);
+                intent.putExtra("mName",mNameEntry.getText().toString());
+                startActivity(intent);
+                finish();
+
+            }
+        });
+            }
 }
