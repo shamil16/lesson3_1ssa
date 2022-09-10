5e7c0231a7494eafe80b941d88dbb2b8574ebb1a
shamil16 <chernovshamil6@gmail.com>
2022-09-10 11:11:01 +0600

    ав

--- app/src/main/java/com/company/SecondActivity.java
+++ app/src/main/java/com/company/SecondActivity.java
@@ -3,12 +3,16 @@ package com.company;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private TextView mName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mName=findViewById(R.id.tv_two);
+        String txtName = getIntent().getStringExtra("parametr");
+        mName.setText(mName.getText().toString()+ " "+txtName);
     }
 }
